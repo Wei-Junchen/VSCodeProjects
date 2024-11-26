2f78583f4c80910b3f8029e4017d888747134eaf
LITWEI <w2718314@gmail.com>
2024-11-26 16:28:05 +0800

    继续新增

--- maze/maze.cpp
+++ maze/maze.cpp
@@ -2,14 +2,17 @@
 #include <random>
 #include <windows.h>
 #include <chrono>
+#include <stack>
+#include <array>
 
-#define width 10
-#define height 10 
+#define DEBUG 0
 
+constexpr std::size_t width{15},height{75};
+std::stack<std::array<int,2>> line;
 int p[1000][3][2]{}; // p[i] -> {{x1,y1},{x2,y2},{x3,y3}}
 int pp[1000]{};
-int line[1000][2]{};
 int table[width][height]{};
+int table_t[width][height]{};
 int setX{},setY{};
 
 void checkAdd(int x,int y,int num,int& index)
@@ -28,16 +31,16 @@ void checkAdd(int x,int y,int num,int& index)
         else if(y==height-1)
             isNotCheck[2]=true;
         if((!isNotCheck[0]))
-            if(table[x+1][y]==1)
+            if(table_t[x+1][y]==1)
                 return ;
         if((!isNotCheck[1]))
-            if(table[x-1][y]==1)
+            if(table_t[x-1][y]==1)
                 return ;
         if((!isNotCheck[2]))
-            if(table[x][y+1]==1)
+            if(table_t[x][y+1]==1)
                 return ;
         if((!isNotCheck[3]))
-            if(table[x][y-1]==1)
+            if(table_t[x][y-1]==1)
                 return ;
     }
     p[num][index][0]=x;
@@ -56,40 +59,55 @@ int getRand(int range)
 
 void f(int x,int y,int num)
 {
+#if !DEBUG
     system("cls");
+#endif
     for(int i=0;i<width;++i)
     {
         for(int j=0;j<height;++j)
             std::cout<<table[i][j]<<" ";
         std::cout<<std::endl;
     }
+#if DEBUG
+    std::cout<<"now:"<<x<<","<<y<<std::endl;
+#endif
     if(x==setX&&y==setY)//找到目标点跳出
         return;
-    if(line[num][0]!=0&&line[num][1]!=0&&num!=0)//说明返回到了上一个点,num!=0确保不会是起始点
+    if(line.size()==num+1)//说明返回到了上一个点,num!=0确保不会是起始点
     {
+#if DEBUG
+        std::cout<<"back:("<<std::endl;
+        for(int i=0;i<pp[num];++i)
+            std::cout<<p[num][i][0]<<","<<p[num][i][1]<<std::endl;
+#endif
         for(int i=0;i<pp[num];++i)
         {
-            if(p[num][i][0]==x&&p[num][i][1]==y)
+            if(p[num][i][0]==x&&p[num][i][1]==y)//这里目前存在问题？
             {
                 --pp[num];
                 p[num][i][0]=0;
                 p[num][i][1]=0;
                 //保证p[num]能够全部密排 !{{0,0},{x1,y1},{x2,y2}};
-                for(int j=i;j<pp[num]-1;++j)
+                for(int j=i;j<pp[num];++j)
                 {
                     p[num][j][0]=p[num][j+1][0];
-                    p[num][j][1]=p[num][j+1][0];
+                    p[num][j][1]=p[num][j+1][1];
                 }
                 break;
             }
         }
+#if DEBUG
+        std::cout<<"after delete:"<<std::endl;
+        for(int i=0;i<pp[num];++i)
+            std::cout<<p[num][i][0]<<","<<p[num][i][1]<<std::endl;
+        system("pause");
+#endif
         //在剩余可能的点中尝试
         if(pp[num]==0)//如果该点pp(可能延伸点)为零
         {
-            table[line[num][0]][line[num][1]]=0;//table清零
-            int xCopy{line[num][0]},yCopy{line[num][1]};
-            line[num][0]=0;
-            line[num][1]=0;
+            table[line.top()[0]][line.top()[1]]=0;//table清零
+            int xCopy{line.top()[0]},yCopy{line.top()[1]};
+            line.pop();
             f(xCopy,yCopy,num-1);
         }
         else//在剩余的点中尝试延伸
@@ -108,8 +126,8 @@ void f(int x,int y,int num)
         }
         else
         {
-            int dx{line[num-1][0]-x};
-            int dy{line[num-1][1]-y};
+            int dx{line.top()[0]-x};
+            int dy{line.top()[1]-y};
             if(dy)//dx==0
             {
                 checkAdd(x+1,y,num,index); 
@@ -124,19 +142,25 @@ void f(int x,int y,int num)
             }
         }
         if(index==0)
+        {
+#if DEBUG
+            std::cout<<"None"<<std::endl;
+            system("pause");
+#endif
             f(x,y,num-1);
+        }
         else
         {
             pp[num]=index;
-            line[num][0]=x;
-            line[num][1]=y;
+            line.push({x,y});
             int chosen=getRand(pp[num]);
             table[x][y]=1;
-            /*for(int i=0;i<pp[num];++i)
-            {
-               std::cout<<p[num][i][0]<<" "<<p[num][i][1]<<"----pp"<<std::endl;
-            }
-            std::cout<<"r:"<<chosen<<std::endl;*/
+            table_t[x][y]=1;
+#if DEBUG
+            for(int i=0;i<pp[num];++i)
+                std::cout<<p[num][i][0]<<","<<p[num][i][1]<<std::endl;
+            system("pause");
+#endif
             f(p[num][chosen][0],p[num][chosen][1],num+1);
         }
     }
@@ -151,6 +175,26 @@ void buildPath(int x,int y)
 
 int main()
 {
-    buildPath(9,9);
+    buildPath(width-1,height-1);
+    std::stack<std::array<int,2>> reverse;
+    std::cout<<"Origin Maze:"<<std::endl;
+    for(int i=0;i<width;++i)
+    {
+        for(int j=0;j<height;++j)
+            std::cout<<table_t[i][j]<<" ";
+        std::cout<<std::endl;
+    }
+    /*std::size_t lineLength=line.size();
+    for(std::size_t i=0;i<lineLength;++i)
+    {
+        reverse.push(line.top());
+        line.pop();
+    }
+    for(std::size_t i=0;i<lineLength;++i)
+    {
+        std::cout<<"("<<reverse.top()[0]<<","<<reverse.top()[1]<<")->";
+        line.push(reverse.top());
+        reverse.pop();
+    }*/
 }
 
